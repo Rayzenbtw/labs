1fcf3beec58b6a95c764a0d9bc77b962207fb184
Volodymyr Shyhimahin <19shigimagin65@gmail.com>
2025-05-01 17:17:19 +0200

    Final changement of logic in individual task 1.

--- Individual/lib.h
+++ Individual/lib.h
@@ -12,10 +12,10 @@
 #define MIN_len 3 // с нулевым символом минимум два символа
 #define MAX_TEXT 1000
 
-void sortAnogramma(char *str);
-bool isValid(char *text);
-void findAnograma(char words[MAX_slov][MAX_len + 1], int wordCount);
-int end(char* word, char* ending);
-void removeln(char words[MAX_slov][MAX_len + 1], int* wordCount, char* ending);
-int splitWords(char* text, char words[MAX_slov][MAX_len + 1]);
+void sortAnogramma(char *str);// принимает указатель на строку
+bool isValid(char *text);// принимает строку, возвращает true если строка удовлетворяет условиям задачи, false если нет
+void findAnograma(char words[MAX_slov][MAX_len + 1], int wordCount);//принимает массив слов и количество слов на входе, выводит анаграммы первого слова
+int end(char* word, char* ending);//Принимает две строки, слово и суфікс, повертає 1 якщо слово закінчується на суфікс, 0 якщо ні
+void removeln(char words[MAX_slov][MAX_len + 1], int* wordCount, char* ending);//Удаляет из массива words все слова, заканчивающиеся на ending, и обновляет значение wordCount, без возврата
+int splitWords(char* text, char words[MAX_slov][MAX_len + 1]);//Разбивает строку text на слова и сохраняет их в массив words, возвращает количество найденных слов.
 #endif //LIB_H
